8febc86b49efd8809098052a34a9abed3b7ecc8d
mayurkasar12 <mayur.kasar12@gmail.com>
2023-09-27 19:12:46 +0530

    First Commit

---
+++ src/main/java/app/Calculator.java
@@ -0,0 +1,16 @@
+package app;
+
+public class Calculator {
+	public int add(int i, int j)
+	{
+		return i+j;
+	}
+	public int multiply(int i,int j)
+	{
+		return i*j;
+	}
+	public int division(int i,int j)
+	{
+		return i/j;
+	}
+}
